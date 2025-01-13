4eb4f822d673ee3e5562b4ff100a8dc1a8e025ae
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2025-01-13 13:52:17 +0530

    feat(info-wall): add date, num. of people occasion field

--- src/components/user-info-modal.tsx
+++ src/components/user-info-modal.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
@@ -13,20 +15,33 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
+import { DatePickerDemo } from '@/components/date-picker-demo';
 
 interface UserInfoModalProps {
-  onSubmit: (data: { name: string; phone: string; address: string }) => void;
+  onSubmit: (data: {
+    name: string;
+    phone: string;
+    address: string;
+    date: Date;
+    numberOfPeople: number;
+    occasion: string;
+  }) => void;
 }
 
 export function UserInfoModal({ onSubmit }: UserInfoModalProps) {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [numberOfPeople, setNumberOfPeople] = useState(10);
+  const [occasion, setOccasion] = useState('');
   const router = useRouter();
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    onSubmit({ name, phone, address });
+    if (date) {
+      onSubmit({ name, phone, address, date, numberOfPeople, occasion });
+    }
   };
 
   const handleClose = () => {
@@ -60,6 +75,7 @@ export function UserInfoModal({ onSubmit }: UserInfoModalProps) {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 className="col-span-3"
+                placeholder={'Your full name'}
                 required
               />
             </div>
@@ -73,23 +89,61 @@ export function UserInfoModal({ onSubmit }: UserInfoModalProps) {
                 value={phone}
                 onChange={(e) => setPhone(e.target.value)}
                 className="col-span-3"
+                placeholder={'Valid 10 digit phone number'}
                 required
               />
             </div>
+
             <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="address" className="text-right">
-                Address
+              <Label htmlFor="date" className="text-right">
+                Date
               </Label>
-              <Textarea
-                id="address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
+              <div className="col-span-3">
+                <DatePickerDemo date={date} setDate={setDate} />
+              </div>
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="numberOfPeople" className="text-right">
+                Number of People
+              </Label>
+              <Input
+                id="numberOfPeople"
+                type="number"
+                value={numberOfPeople}
+                onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
                 className="col-span-3"
+                placeholder={'Minimum 10'}
+                required
+                min={10}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="occasion" className="text-right">
+                Occasion
+              </Label>
+              <Input
+                id="occasion"
+                value={occasion}
+                onChange={(e) => setOccasion(e.target.value)}
+                className="col-span-3"
+                placeholder={'Eg. Wedding, Birthday, etc.'}
                 required
               />
             </div>
           </div>
-          <DialogFooter>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="address" className="text-right">
+              Address
+            </Label>
+            <Textarea
+              id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              placeholder={'Optional'}
+              className="col-span-3"
+            />
+          </div>
+          <DialogFooter className={'mt-4'}>
             <Button type="submit">Submit</Button>
           </DialogFooter>
         </form>
